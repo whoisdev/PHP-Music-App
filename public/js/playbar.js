77a3223a3359501b1f5d1d58020134578beee5ca
dev1203 <devsingh120397@gmail.com>
2018-11-12 12:32:17 -0800

    Bug fix:

--- public/js/playbar.js
+++ public/js/playbar.js
@@ -3,11 +3,13 @@ new Vue({
     methods : {
         updatesong : function(event){
             let button = $('#html_player')[0];
-            var $div = $(event.target);
-            var offset = $div.offset();
-            var x = event.clientX - offset.left;
-            x = ((x/$div.width()) * button.duration);
-            button.currentTime = x;
+                if(button.duration){
+                var $div = $(event.target);
+                var offset = $div.offset();
+                var x = event.clientX - offset.left;
+                x = ((x/$div.width()) * button.duration);
+                button.currentTime = x;
+            }
         },
         onTimeUpdateListener : function(){
             let button = $('#html_player')[0];
--- public/js/playlist.js
+++ public/js/playlist.js
@@ -9,26 +9,26 @@ new Vue({
             $('.playlist_name').fadeIn();
             let button = event.target;
             button.id = 'submit_playlist';
-            this.$http({
-                method : 'POST',
-                url : `${site_url}/playlist/add`,
-                data : {name:$('.playlist_name').val()} 
-            })
-            .then(response =>{
-                $('body').append(response);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-            // $("#submit_playlist").on('click',()=>{
-            //     $.post(`${site_url}/playlist/add`,{name:$('.playlist_name').val()},(data)=>{
-            //         $('body').append(data);
-            //         setTimeout(()=>{
-            //             $('.message').fadeOut();
-            //             location.reload();
-            //         },500);
-            //     });
-            // });
+            // this.$http({
+            //     method : 'POST',
+            //     url : `${site_url}/playlist/add`,
+            //     data : {name:$('.playlist_name').val()} 
+            // })
+            // .then(response =>{
+            //     $('body').append(response);
+            // })
+            // .catch((err) => {
+            //     console.log(err);
+            // })
+            $("#submit_playlist").on('click',()=>{
+                $.post(`${site_url}/playlist/add`,{name:$('.playlist_name').val()},(data)=>{
+                    $('body').append(data);
+                    setTimeout(()=>{
+                        $('.message').fadeOut();
+                        location.reload();
+                    },500);
+                });
+            });
         }
     }
 });
